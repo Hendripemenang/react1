cf7b18bb26cd4eda5e7490eb80052dc5ad0f9c4a
hendri <Hendribless87@gmail.com>
2022-11-16 21:41:49 +0700

    kosong2

--- src/App.js
+++ src/App.js
@@ -1,32 +1,8 @@
 function App(){
-    const title='Blog Post'
-    const body='this is my blog post'
-    const comment =[
-        {id:1, text:'Coment one'},
-        {id:2, text:'Coment two'},
-        {id:3, text:'Coment three'},
-    ]
-    const loading=false
-    const showCommment =true
-    if(loading) return <h1>loading...</h1>
-
-    const commentBlock =(
-        <div className="comment">
-            <h3>Coments({comment.length}) </h3>
-            <ul>
-                {comment.map((comment,index)=>
-                    (<li key={index}>{comment.text}</li>))}
-            </ul>
-        </div>
-    )
 
     return (
         <div className='container'>
-            <h1>{title.toUpperCase()}</h1>
-            <p>{body}</p>
-            {showCommment && (commentBlock) }
-
-
+            <h1> My App</h1>
 
         </div>
 
